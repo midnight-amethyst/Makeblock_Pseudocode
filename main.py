5edd336981a288ce7d33cd64b13828038ac27452
midnight-amethyst <marekwierciak785@gmail.com>
2025-02-24 13:26:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from robot import *
 motor1 = Motor('motor1')
 motor2 = Motor('motor2')
 motorArm = Motor('Arm')
-<<<<<<< Updated upstream
-=======
+
 sensor = Sensor()
 
 
@@ -20,13 +19,12 @@ def turn():
 def grab():
     # Grab
     # Arm up
-    
+
     return True
 
 def release():
     pass
 
->>>>>>> Stashed changes
 sensor = Sensor()
 
 
@@ -37,4 +35,40 @@ def move():
     pass
 
 while True:
-    pass
+
+
+    counter += 1
+
+    # Move forwards until distance is close enoguh
+    # Grab (switch modes)
+    # Find the box by rotating
+    # Move forwards towards box
+    # Release
+
+    distance = sensor.measure_distance()
+
+    # Move initial
+    if distance >= 10 and counter < 30:
+        move()
+    elif distance < 10 and counter < 30:
+        motor1.stop()
+        motor2.stop()
+        grab()
+    elif counter >= 30 and counter < 40:
+        turn()
+        # Provide data
+    elif counter >= 40 and counter < 50 and distance + 1> "provided distance":
+        turn()
+    elif distance <= "provided distance" + 1:
+        motor1.stop()
+        motor2.stop()
+    elif counter >= 50 and distance >= 10:
+        move()
+    elif counter >= 50 and distance < 10:
+        motor1.stop()
+        motor2.stop()
+        release()
+
+
+
+

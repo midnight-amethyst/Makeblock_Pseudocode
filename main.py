4f6a00a24e4ff6f246b603b50ce609ff86fcaf52
midnight-amethyst <marekwierciak785@gmail.com>
2025-02-24 12:22:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,30 @@ from robot import *
 motor1 = Motor('motor1')
 motor2 = Motor('motor2')
 motorArm = Motor('Arm')
+<<<<<<< Updated upstream
+=======
+sensor = Sensor()
+
+
+counter = 0
+
+
+def move():
+    pass
+
+def turn():
+    pass
+
+def grab():
+    # Grab
+    # Arm up
+    
+    return True
+
+def release():
+    pass
+
+>>>>>>> Stashed changes
 
 while True:
     pass
